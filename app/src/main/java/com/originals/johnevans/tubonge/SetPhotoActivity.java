4f77077b27929d41cd6bbb34febc6d1ff2b6b85c
John <johngachihi3@gmail.com>
2017-04-18 22:57:38 +0300

    eehhh

--- app/src/main/java/com/originals/johnevans/tubonge/SetPhotoActivity.java
+++ app/src/main/java/com/originals/johnevans/tubonge/SetPhotoActivity.java
@@ -26,6 +26,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -34,6 +35,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -46,9 +48,10 @@ import static android.os.Environment.getExternalStoragePublicDirectory;
 public class SetPhotoActivity extends AppCompatActivity {
 
     ImageView setPhoto;
+    ImageView imageView2;
     Button setPhoto_bt;
-    Context context;
-    static Uri uri1;
+   // Context context;
+    public static Uri uri1;
     static Bitmap photo;
     static Bitmap photophoto;
 
@@ -59,6 +62,7 @@ public class SetPhotoActivity extends AppCompatActivity {
         final Context context = this;
 
         setPhoto = (ImageView) findViewById(R.id.imageView);
+        imageView2 = (ImageView) findViewById(R.id.imageView2);
 
         setPhoto_bt = (Button) findViewById(R.id.setPhoto_bt);
         setPhoto_bt.setOnClickListener(new View.OnClickListener() {
@@ -66,7 +70,26 @@ public class SetPhotoActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent takePhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (takePhoto.resolveActivity(getPackageManager()) != null) {
-                    startActivityForResult(takePhoto, 1);
+                    File file = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                   /* if (new File(file, "my_icon_tubongee.jpg").exists()) {
+                        if (new File(file, "my_icon_tubongee.jpg").delete()) {
+                            Toast.makeText(getApplicationContext(), "deleted", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                    File imageFile = new File(file, "my_icon_tubongee.jpg");*/
+                   File imageFile = null;
+                    try {
+                        imageFile = File.createTempFile("icon", ".jpg", file);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    if (imageFile != null) {
+                        uri1 = Uri.fromFile(imageFile);
+                        takePhoto.putExtra(MediaStore.EXTRA_OUTPUT, uri1);
+                        startActivityForResult(takePhoto, 1);
+                    }
                 }
             }
         });
@@ -75,105 +98,50 @@ public class SetPhotoActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == 1) {
-            Bundle photo_b = data.getExtras();
-            Bitmap photo;
-            photo = (Bitmap) photo_b.get("data");
-            setPhoto = (ImageView) findViewById(R.id.imageView);
-            setPhoto.setImageBitmap(photo);
-            new SetIconAsync().execute(photo);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Picasso.with(getApplicationContext()).load(uri1).resize(50, 50).centerCrop().into(setPhoto);
         }
+        File imageFile = new File(uri1.getPath());
+        Bitmap image = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+        imageView2.setImageBitmap(image);
+
+        String imageString = getStringImage(image);
+        uploadImage(imageString);
     }
 
-
-
-    public class SetIconAsync extends AsyncTask<Bitmap, Void, Bitmap> {
-
-       // Bitmap photo;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected Bitmap doInBackground(Bitmap[] params) {
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            params[0].compress(Bitmap.CompressFormat.JPEG, 50, baos);
-            byte[] imageByte = baos.toByteArray();
-            String encodedImage = Base64.encodeToString(imageByte, Base64.DEFAULT);
-            Bitmap imagephoto = postIcon(encodedImage);
-            return imagephoto;
-        }
-
-        @Override
-        protected void onProgressUpdate(Void[] values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap b) {
-            ImageView imageView = (ImageView) findViewById(R.id.imageView2);
-            imageView.setImageBitmap(b);
-
-        }
-
-        public Bitmap postIcon(final String encodedImage) {
-            Log.e("postIcon", "post iconning");
-            final Bitmap[] photo = new Bitmap[1];
-            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-            String url = "http://192.168.0.13/tubonge_app/IconSet.php";
-            StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    try {
-                        JSONObject jsonObject = new JSONObject(response);
-                        boolean error = jsonObject.getBoolean("error");
-                        if (!error) {
-                            JSONObject userJson = jsonObject.getJSONObject("user");
-                            String image = userJson.getString("icon");
-
-                            byte[] imageByte = image.getBytes();
-                            BitmapFactory.Options options = new BitmapFactory.Options();
-                            options.inPurgeable = true;
-                            photo[0] = BitmapFactory.decodeByteArray(imageByte, 0, imageByte.length, options);
-                            if (photo[0] == null) {
-                                Log.e("photo is null", "true");
-                            } else {
-                                Log.e("photo is null", "false");
-                            }
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                        Toast.makeText(getApplicationContext(), e.toString()+"---"+e.getMessage(), Toast.LENGTH_LONG).show();
-                    }
-
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(getApplicationContext(), error.toString()+"---"+error.getMessage(), Toast.LENGTH_LONG).show();
-                }
-            }) {
-                @Override
-                protected Map<String, String> getParams() throws AuthFailureError {
-                    HashMap<String, String> params = new HashMap<>();
-                    params.put("icon", encodedImage);
-                    SharedPreferences preferences = getSharedPreferences("user_pref", Context.MODE_PRIVATE);
-                    String userid = preferences.getString("userid", null);
-                    params.put("userid", userid);
-                    return params;
-                }
-            };
-
-            requestQueue.add(request);
-            String bool;
-            if (photo != null) {
-                bool = "false";
-            } else {bool = "true";}
-            Log.e("is photo null?", bool);
-            return photo[0];
-        }
+    public String getStringImage(Bitmap b) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        b.compress(Bitmap.CompressFormat.JPEG, 50, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return imageString;
     }
 
+    public void uploadImage(final String imageString) {
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        String url = "http://192.168.0.13/tubonge_app/IconSet.php";
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                SharedPreferences pref = getSharedPreferences("user_pref", MODE_PRIVATE);
+                String userid = pref.getString("userid", null);
+                HashMap<String, String> params = new HashMap<>();
+                params.put("icon", imageString);
+                params.put("userid", userid);
+                return params;
+            }
+        };
+
+        requestQueue.add(request);
+    }
 }
